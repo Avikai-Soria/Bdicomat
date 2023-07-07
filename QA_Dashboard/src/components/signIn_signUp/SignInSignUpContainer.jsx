b873247a6c9b5ea8488e05097e6a4cb9bbe380a0
chananelz <chananelz@gmail.com>
2023-07-07 19:26:37 +0300

    end first stage

--- QA_Dashboard/src/components/signIn_signUp/SignInSignUpContainer.jsx
+++ QA_Dashboard/src/components/signIn_signUp/SignInSignUpContainer.jsx
@@ -7,7 +7,6 @@ import pyTestLogo from "../../assets/img/Pytest_logo.svg";
 import seleniumLogo from "../../assets/img/selenium_logo.svg";
 import sing_in_main_logo from "../../assets/img/sing_in_main_logo.svg";
 import sing_up_main_logo from "../../assets/img/sing_up_main_logo.svg";
-import test_logo from "../../assets/img/test_logo.svg";
 
 import "../../style_files/SignInSignUpContainer.css";
 
@@ -41,21 +40,13 @@ export default function SignInSignUpContainer() {
                 <input type="password" placeholder="Password" />
               </div>
               <input type="submit" value="Login" className="btn solid" />
-              <p className="social-text">Or Sign in with social platforms</p>
-              <div className="social-media">
-                <a href="#" className="social-icon">
-                  <i className="fab fa-facebook-f"></i>
-                </a>
-                <a href="#" className="social-icon">
-                  <i className="fab fa-twitter"></i>
-                </a>
-                <a href="#" className="social-icon">
-                  <i className="fab fa-google"></i>
-                </a>
-                <a href="#" className="social-icon">
-                  <i className="fab fa-linkedin-in"></i>
-                </a>
-              </div>
+              <p className="testing-text">New! integration with the most powerfully testing frameworks:</p>
+              <dev className="testing-media">
+                <img src={jenkinsLogo} className="testing-icon" alt="" />
+                <img src={playWrightLogo} className="testing-icon" alt="" />
+                <img src={pyTestLogo} className="testing-icon" alt="" />
+                <img src={seleniumLogo} className="testing-icon" alt="" />
+              </dev>
             </form>
             <form action="#" className="sign-up-form">
               <h2 className="title">Sign up</h2>
@@ -103,7 +94,12 @@ export default function SignInSignUpContainer() {
                 Sign up
               </button>
             </div>
-            <img src={sing_in_main_logo} className="image" alt="" />
+            <img
+              src={sing_in_main_logo}
+              className="image"
+              alt=""
+              height="500"
+            />
           </div>
           <div className="panel right-panel">
             <div className="content">
