d31f79ad74d57ced760bf013cf971ee13308b4d6
chananelz <chananelz@gmail.com>
2023-07-09 00:29:21 +0300

    before refactored

--- QA_Dashboard/src/components/signIn_signUp/SignInSignUpContainer.jsx
+++ QA_Dashboard/src/components/signIn_signUp/SignInSignUpContainer.jsx
@@ -60,20 +60,15 @@ export default function SignInSignUpContainer() {
                 <input type="password" placeholder="Password" />
               </div>
               <input type="submit" className="btn" value="Sign up" />
-              <p className="social-text">Or Sign up with social platforms</p>
-              <div className="social-media">
-                <a href="#" className="social-icon">
-                  <i className="fab fa-facebook-f"></i>
-                </a>
-                <a href="#" className="social-icon">
-                  <i className="fab fa-twitter"></i>
-                </a>
-                <a href="#" className="social-icon">
-                  <i className="fab fa-google"></i>
-                </a>
-                <a href="#" className="social-icon">
-                  <i className="fab fa-linkedin-in"></i>
-                </a>
+              <p className="testing-text">
+                We are trusted by the most famous companies:
+              </p>
+              <div className="testing-media">
+                <img className="pyTestLogo testing-icon" alt="" />
+                <img className="seleniumLogo testing-icon" alt="" />
+                <img className="cypressLogo testing-icon" alt="" />
+                <img className="testCafeLogo testing-icon" alt="" />
+                <img className="junitLogo testing-icon" alt="" />
               </div>
             </form>
           </div>
@@ -84,8 +79,9 @@ export default function SignInSignUpContainer() {
             <div className="content">
               <h3>New here ?</h3>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Debitis, ex ratione. Aliquid!
+                Welcome to AutoTesting! Join today thousands of satisfied
+                customers who are using our platform to visualize their QA
+                processes.
               </p>
               <button
                 onClick={() => transparentSignUp()}
@@ -105,10 +101,7 @@ export default function SignInSignUpContainer() {
           <div className="panel right-panel">
             <div className="content">
               <h3>One of us ?</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-                laboriosam ad deleniti.
-              </p>
+              <p>For an easy and secure connection, click here.</p>
               <button
                 onClick={() => transparentSignIn()}
                 className="btn transparent"
