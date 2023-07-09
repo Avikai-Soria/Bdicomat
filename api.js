26364c0bf6f22c491f3e31f89de617fa94e39cac
chananelz <chananelz@gmail.com>
2023-07-09 11:25:58 +0300

    work with avchy

--- api.js
+++ api.js
@@ -20,3 +20,25 @@ export default async function apiFetch(route, method, apiKey, body) {
       throw error;
     }
   }
+
+  export default async function apiFetchWithoutAuthor(route, method,  body) {
+    const url = `http://localhost:2999/${route}`;
+    try {
+      const response = await fetch(url, {
+        method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: body ? JSON.stringify(body) : undefined,
+      });
+
+      if (response.ok) {
+        return await response.json();
+      } else {
+        throw new Error(`Request failed with status ${response.status}`);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      throw error;
+    }
+  }
