78c65b89754a707c155c3f6b15abb9ac41cc9922
Avikaielef <soria@g.jct.ac.il>
2023-07-24 13:35:33 +0300

    Found the issue

--- api/query/domain.query.js
+++ api/query/domain.query.js
@@ -2,9 +2,9 @@ const QUERY = {
   SELECT_DOMAIN_STATS: `
     SELECT
       domain,
-      SUM(CASE WHEN status = 'pass' THEN 1 ELSE 0 END) AS testsPassed,
-      SUM(CASE WHEN status = 'fail' THEN 1 ELSE 0 END) AS testsFailed,
-      SUM(CASE WHEN status = 'running' THEN 1 ELSE 0 END) AS testsRunning,
+      SUM(CASE WHEN result = 'pass' THEN 1 ELSE 0 END) AS testsPassed,
+      SUM(CASE WHEN result = 'fail' THEN 1 ELSE 0 END) AS testsFailed,
+      SUM(CASE WHEN result = 'running' THEN 1 ELSE 0 END) AS testsRunning,
       SUM(CASE WHEN isExcepted = 'yes' THEN 1 ELSE 0 END) AS bugsExpected,
       SUM(CASE WHEN isExcepted = 'no' THEN 1 ELSE 0 END) AS bugsUnexpected
     FROM
